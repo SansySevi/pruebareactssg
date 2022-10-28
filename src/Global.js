e01b1b00653850c7d73af27c9d85d37a1698dbd8
SansySevi <ssevilleja@hotmail.es>
2022-10-28 12:52:17 +0200

    init

---
+++ src/Global.js
@@ -0,0 +1,5 @@
+var Global = {
+    
+}
+
+export default Global;
---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Popper  from 'popper.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
